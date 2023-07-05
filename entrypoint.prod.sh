bf6cb3baa2f03240a43a8d2704343490a7260fcc
kacperpy <kacperpy@gmail.com>
2023-07-05 14:41:06 +0200

    entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -16,5 +16,4 @@ exec "$@"
 python manage.py makemigrations
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --username admin --email admin@admin.com --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin'); user.set_password('1234'); user.save()"
+python manage.py createsuperuser --username 'admin' --email 'admin@admin.com' --noinput
