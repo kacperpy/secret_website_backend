243a5808714e28a316cab5359b21847151d38fd7
kacperpy <kacperpy@gmail.com>
2023-07-05 14:25:57 +0200

    entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+python manage.py makemigrations
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+python manage.py createsuperuser --username admin --email admin@admin.com --noinput
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='admin'); user.set_password('1234'); user.save()"
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
