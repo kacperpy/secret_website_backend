14100225585b288ef170af2bdbce5b747697d2e4
kacperpy <kacperpy@gmail.com>
2023-07-05 15:05:19 +0200

    managepy

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -9,16 +9,6 @@ then
     done
 
     echo "PostgreSQL started"
-    python manage.py makemigrations
-    python manage.py migrate --no-input
-    python manage.py collectstatic --no-input
-
-    username="admin"
-    email="admin@admin.com"
-    password="1234"
-
-    python manage.py createsuperuser --username $username --email $email --noinput
-    python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='$username'); user.set_password('$password'); user.save()"
 fi
 
 exec "$@"
